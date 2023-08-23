80fdc989d31562fd99427cbe2f000780f88f925d
Neoman9 <nelsonbenedict35@yahoo.com>
2023-08-23 15:28:59 +0100

    EDA

---
+++ phishing/component/data_transformation.py
@@ -0,0 +1,12 @@
+from phishing.exception import PhishingException
+from phishing.logger import logging
+from phishing.constants import *
+from phishing.entity.config_entity import DataTransformationConfig
+from phishing.entity.artifact_entity import DataTransformationArtifact, DataIngestionArtifact,DataValidationArtifact
+
+
+import os, sys 
+import pandas as pd 
+
+
+
